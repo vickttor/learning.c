b9bd3d050bdd8d894da357528682b75b737ae433
Victor Silva <victor.silvaPC@hotmail.com>
2023-05-09 23:32:15 -0300

    feat: :bug: fixed the issues in statement file.

--- src/statements.c
+++ src/statements.c
@@ -10,13 +10,13 @@ int main() {
   // if = A statement to use with short else if cases
 
   if(age >= 18) {
-    printf("You are now signed up!\n");
+    printf("- You are now signed up!");
   }else if(age < 0) {
-    printf("Are you okay bro '_' ?\n");
+    printf("- Are you okay bro '_' ?");
   }else if(age == 0){
-    printf("You can't sign up! You were just born!\n");
+    printf("- You can't sign up! You were just born!");
   }else {
-    printf("You are too young to sign up!\n");
+    printf("- You are too young to sign up!");
   }
 
   // switch = A more efficient alternative to using many "else if"
@@ -24,27 +24,27 @@ int main() {
 
   char grade;
 
-  printf("Enter a letter grade: ");
-  scanf(" %c", &grade);
+  printf("\n\nEnter a letter grade: ");
+  scanf("%s", &grade);
 
   switch (grade){
     case 'A':
-      printf("Good Job |^o^| !\n");
+      printf("- Good Job |^o^| !\n");
       break;
     case 'B':
-      printf("Congratultions ^_^\n");
+      printf("- Congratultions ^_^\n");
       break;
     case 'C':
-      printf(":) Hmmm ok....\n");
+      printf("- :) Hmmm ok....\n");
       break;
     case 'D':
-      printf("Oh sorry, good luck in the next\n");
+      printf("- Oh sorry, good luck in the next\n");
       break;
     case 'F': 
-      printf("wtf bro '_'... wha...\n");
+      printf("- wtf bro '_'... \n");
       break;
     default:
-      printf("Please enter only valid grades\n");
+      printf("- Please enter only valid grades\n");
   }
 
   return 0;
