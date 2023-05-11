b12beddbd9681a94f6544033b12bed703203fa87
Victor Silva <victor.silvaPC@hotmail.com>
2023-05-11 20:17:11 -0300

    fix: :zap: added advantages section

--- src/functionPrototype.c
+++ src/functionPrototype.c
@@ -5,14 +5,19 @@ void hello(char[], int); // function prototype
 int main() {
   // function prototype
 
-  // What is It?
+  // WHAT IS IT?
   // function declaration, w/o a body, before main()
   // Ensures that calls to a function are made with the correct arguments
 
-  // Important notes
+  // IMPORTANT NOTES
   // Many C compilers do not check for parameter matching
   // Missing arguments will result in unexpected behavior
   // a function prototype causes the compiler to flag an error if argument are missing
+
+  // ADVANTAGES
+  // 1. Easier to navigate a program w/ main at the top
+  // 2. Helps with debugging
+  // 3. Commonly used in header files
 
   char name[] = "Victor";
   int age = 18;
