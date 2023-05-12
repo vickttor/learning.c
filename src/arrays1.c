c9da6b4b709695ad36c5ce71bdd51f4830e8241c
Victor Silva <victor.silvaPC@hotmail.com>
2023-05-11 22:54:40 -0300

    feat: iterating through array with sizeof

--- src/arrays1.c
+++ src/arrays1.c
@@ -5,10 +5,25 @@ int main() {
 
   double prices[6] = {5.0, 10.0, 15.0, 25.0, 20.0}; // assigning in declaration
   prices[5] = 34.5; // assigning later
+  printf("$%.2lf\n", prices[0]);
 
-  int index = 5;
+  // iterating through the array using the sizeof operator
+  
+  /**
+   * The sizeof calculates the amount of bytes. So
+   * If we have an array of 6 elements which are double
+   * that means we have 6 * 8 bytes (since the double is 8B)
+   * what results in 48 bytes.
+   * 
+  */
+  printf("sizef of pricess array: %d\n", sizeof(prices));
 
-  printf("prices[%d] = $%.2lf\n", index, prices[index]);
+  // sizeof(prices)/sizeof(prices[0]) is the same that:
+  // 48/8 = 6
+
+  for(int i = 0; i < sizeof(prices)/sizeof(prices[0]); i++) {
+    printf("prices[%d] = %.2lf\n", i, prices[i]);
+  }
 
   return 0;
 }
